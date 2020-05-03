ce73284020b7b4e2a243aa32bf773038bc1a8430
Le Minh Duc <55783362+zaqy203@users.noreply.github.com>
2020-05-03 21:09:50 +0700

    Update Qfloat.h

--- SoNguyenLon/Qfloat.h
+++ SoNguyenLon/Qfloat.h
@@ -6,12 +6,14 @@ using namespace std;
 class Qfloat
 {
 	int* data = new int[4];
+public:
 	Qfloat();
 	bool getBit(int index);
 	void setBit(int index, bool bit);
 	void ScanQfloat();
-	void PrintQfloat(Qfloat );
-	Qfloat BinToDec(int*);
+	void PrintQfloat();
+	string BinToDec(string);
 	string DecToBin(string);	 
+	Qfloat operator=(Qfloat x);
 };
 
